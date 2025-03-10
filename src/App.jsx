a4603fb1b60d74e3e7078e4a709d60fbb5414e64
Arash <arashsaleh798@gmail.com>
2025-03-10 14:53:44 -0700

    import complete for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import NavBar from "./components/NavBar/NavBar";
+import MailboxList from "./components/MailboxList/MailboxList";
+import MailboxForm from "./components/MailboxForm/MailboxForm";
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
+
+
 const App = () => {
   return <h1>Hello world!</h1>;
 };
